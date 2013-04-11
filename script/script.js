418ce59f66c08c685d661d7b295adb99d68ad41c
Ali Mihandoost <i@ali.md>
2013-04-11 18:41:42 +0430

    Init js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,24 @@
-// Put your custom code here
-setTimeout(function(){
-	window.scrollTo(0,1);
-},1);
+(function(){
+  var
+    onLoad = function(){ // win onload
+      document.addEventListener("deviceready", init, false);
+    },
+
+    fail = function(evt) { // log errors
+      console.log(evt.target.error.code);
+    },
+
+    init = function(){ // deviceready 
+      var
+        fileExist = function(url){ // check file exist
+          return false;
+        };
+
+      alert(fileExist());
+    };
+
+  setTimeout(function(){
+    window.scrollTo(0,1);
+  },1);
+
+})(); // bigbang
