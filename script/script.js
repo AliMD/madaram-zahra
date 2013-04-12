8bd8e304f536b662ae67a205c9fbfe223aa2f1a9
Ali Mihandoost <i@ali.md>
2013-04-12 20:08:08 +0430

    Js Revolution

--- script/script.js
+++ script/script.js
@@ -1,24 +1,100 @@
-(function(){
-  var
-    onLoad = function(){ // win onload
-      document.addEventListener("deviceready", init, false);
+/*jshint strict:true, es5:true, forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, nonew:true, browser:true, devel:true, indent:2, boss:true, curly:false, immed:false, latedef:true, newcap:true, plusplus:false, trailing:true, maxparams:3, maxerr:100, debug:false, asi:false, evil:false, expr:true, eqnull:false, esnext:false, funcscope:false, globalstrict:false, loopfunc:false */
+
+/*
+ * Madaram Zahra Mobile App v1b
+ */
+
+var
+  // Easy log
+  log: function(x){
+    console.log(x);
+  };
+
+(function($,undefined){
+  "use strict";
+
+  // Application Main Object
+  var app = {
+    // Application Resources Url
+    urls: {
+      base: 'file:///android_asset/www/',
+      audio: 'audio/'
     },
-
-    fail = function(evt) { // log errors
-      console.log(evt.target.error.code);
+    // Application Constructor
+    initialize: function() {
+      this.bindEvents();
     },
+    ,
+    // Log errors 
+    error: function(msg) { // log errors
+      log('Err : '+(msg['code']?msg.code:msg));
+    },
+    // Convert audio file name to absolute audio url
+    audioUrl: function(fileName){
+      return urls.base+urls.audio+fileName+'.mp3';
+    },
+    // Bind Event Listeners
+    bindEvents: function() {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+      document.addEventListener('online', this.online, false);
+      document.addEventListener('offline', this.offLine, false);
+    },
+    // deviceready Event Handler
+    onDeviceReady: function() {
+      log('Device Ready');
+      this.playAudio('startup');
+    },
+    // online Event Handler
+    onLine: function(){
+      log('Device OnLine');
+    },
+    // offline Event Handler
+    offLine: function(){
+      log('Device OffLine');
+    },
+    // Media Player Obj
+    mediaPlayer: null,
+    // Play Background Audio
+    playAudio: function(fileName){
+      if(mediaPlayer){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+      }
+      fileName = audioUrl(fileName);
+      log('Play Audio '+fileName);
+      this.mediaPlayer = new Media(fileName);
+    },
+    // Device file system
+    fileSystem: null,
+    // Request the persistent file system
+    reqFileSystem: function (continueFn) {
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        app.fileSystem = fileSystem;
+        log('fileSystem Ready, rootPath: '+app.fileSystem.root.fullPath);
+        continueFn();
+      },
+      function(evt){
+        error('Can not access file system with error code '+evt.target.error.code);
+      });
+    },
+    // check file exist with ajax
+    fileExist: function(url,useAjax,success,error){
+      log('Check file exist : ' + url + ' with '+ useAjax?'ajax':'cordova' );
+      return useAjax ?//if
+        $.ajax({
+          type: 'HEAD',
+          url: url,
+          success: success,
+          error: error
+        }) :// else
+        app.fileSystem.root.getFile(url, {create:false}, success, error);
+    }
 
-    init = function(){ // deviceready 
-      var
-        fileExist = function(url){ // check file exist
-          return false;
-        };
+  }; // end app obj
 
-      alert(fileExist());
-    };
+  // start app
+  app.initialize();
 
-  setTimeout(function(){
-    window.scrollTo(0,1);
-  },1);
-
-})(); // bigbang
+  // Globalization
+  window.app = app;
+})(window.jQuery); // bigbang
